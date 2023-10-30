6ded654a64eed0fc5f67d972935ca187a71a7918
JUDSON LUAN DA SILVA DIAS <20221170150036@ifrn.local>
2023-10-30 14:29:05 -0300

    blabla
    ahgdsfhajgfd

---
+++ jujucriativo/Assets/Materials/player.cs
@@ -0,0 +1,45 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class player : MonoBehaviour
+{
+    public int velocidade = 10;
+    public int forcaPulo = 7;
+    private Rigidbody rb;
+    public bool noChao;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TryGetComponent(out rb);
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.tag == "Chao"){
+            noChao = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        Vector3 direcao = new Vector3(h,0, v);
+        rb.AddForce(direcao * velocidade * Time.deltaTime,ForceMode.Impulse);
+
+        if(Input.GetKeyDown(KeyCode.Space) && noChao){
+            rb.AddForce(Vector3.up * forcaPulo, ForceMode.Impulse);
+        noChao = false;
+        }
+
+        if(transform.position.y <= -10){
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
